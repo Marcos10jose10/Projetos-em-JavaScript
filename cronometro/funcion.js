cf1105e3064142bf357cb2071e07a92727173522
Marcos10jose10 <115882912+Marcos10jose10@users.noreply.github.com>
2022-10-26 23:18:09 -0300

    meu primeiro projeto feitor sozinho

---
+++ cronometro/funcion.js
@@ -0,0 +1,56 @@
+let minuto = document.getElementById('minuto')
+let segundo = document.getElementById('segundo')
+let hora = document.getElementById('hora')  
+
+let hh = 0 
+let mm = 0 
+let ss = 0
+let temp;
+
+function play(){
+    temp = setInterval(() => { tempo(); }, 1000)
+    
+    document.getElementById('play').disabled = true
+    document.getElementById('play').style.opacity = "0.5"
+    document.getElementById('stop').style.opacity = "1"
+    document.getElementById('reset').style.opacity = "1"
+}
+function pausar(){
+    clearInterval(temp)
+    document.getElementById('play').disabled = false
+    document.getElementById('play').style.opacity = "1"
+    document.getElementById('stop').style.opacity = "0.5"
+}
+function reset(){
+    clearInterval(temp)
+    hh = 0
+    mm = 0
+    ss = 0
+    
+    hora.innerHTML = '00'
+    minuto.innerHTML = '00' 
+    segundo.innerText = '00'
+    document.getElementById('play').disabled = false
+    document.getElementById('play').style.opacity = "1"
+    document.getElementById('reset').style.opacity = "0.5"
+    
+} 
+function tempo(){
+    ss++
+    if(ss == 59){
+        
+        ss = 00
+        mm++
+    }
+    if(mm == 59){
+        mm = 00
+        hh++
+    }
+    let cronometroSS = (ss < 10 ? '0' + ss : ss);
+    let cronometroMM= (mm < 10 ? '0' + mm : mm);
+    let cronometroHH = (hh < 10 ? '0' + hh : hh);
+    
+    segundo.innerText = cronometroSS
+    minuto.innerText = cronometroMM
+    hora.innerText = cronometroHH
+}
